df4dd7e0f612849fd4378bce90bda5e468731544
NavinaV23 <navinavbtechit>
2021-12-16 08:02:57 +0530

    done

---
+++ src/org/test/SourceCodeJava.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class SourceCodeJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("sun");
+		
+		System.out.println("mercury");
+		
+		System.out.println("venus");
+		
+	}
+	
+}
